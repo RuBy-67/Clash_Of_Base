9c0a059c04567a804b4a7799b6cf9c063d54d7ac
RuBy-67 <beyer.ruben67@gmail.com>
2023-02-05 18:47:00 +0100

    new file:   src/SlashCommands/compo/c10.js

---
+++ src/SlashCommands/compo/c7.js
@@ -0,0 +1,88 @@
+const { MessageEmbed } = require("discord.js");
+const emo = require(`../../jsons/emoji.json`);
+
+module.exports = {
+  type: 2,
+  name: "hdv7",
+  description: "test",
+  options: [
+    {
+      type: 1,
+      name: "queen_walk",
+      description: "test",
+      options: [
+        {
+          type: "STRING",
+          name: "liste",
+          description: "liste of queen walk compo",
+          choices: [
+            {
+              name: "HGHB",
+              value: "c7hghb",
+            },
+          ],
+          required: true,
+        },
+      ],
+    },
+    {
+      type: 1,
+      name: "ballon",
+      description: "ballon",
+      options: [
+        {
+          type: "STRING",
+          name: "liste",
+          description: "liste",
+          choices: [
+            {
+              name: "compo1",
+              value: "compo1",
+            },
+          ],
+          required: true,
+        },
+      ],
+    },
+  ],
+  run: async (client, interaction, args) => {
+    function emoji(id) {
+      return client.emojis.cache.get(id).toString();
+    }
+    const code = await interaction.options.getString("liste");
+    const embed = new MessageEmbed()
+    
+    if (code == "") {
+
+        return interaction.reply({ embeds: [embed] });
+    }
+    
+
+      .setTitle("Help Compo th7")
+      .setDescription("")
+      .addFields(
+        {
+          name: "",
+          value: "",
+        },
+        {
+          name: "",
+          value: "",
+        },
+        {
+          name: "",
+          value: "",
+          inline: true,
+        }
+      )
+      .setColor("GREEN")
+      .setThumbnail("")
+      .setFooter({
+        text: "RuBy_",
+        iconURL: "",
+      })
+      .setTimestamp(Date.now());
+
+    return interaction.reply({ embeds: [embed] });
+  },
+};
