408634168827ef201d03b8f990eea22f8b883dad
RuBy-67 <beyer.ruben67@gmail.com>
2023-02-26 13:33:31 +0100

    deleted:    src/SlashCommands/constructor/fond2.jpg 	modified:   src/SlashCommands/constructor/img.js

--- src/SlashCommands/constructor/img.js
+++ src/SlashCommands/constructor/img.js
@@ -22,7 +22,9 @@ module.exports = {
       const sorts = sortsStr.split("-");
 
       // charger l'image de fond
-      const background = await loadImage(path.resolve(__dirname, "fond2.jpg"));
+      const background = await loadImage(
+        path.resolve(__dirname, "img/fond2.jpg")
+      );
 
       // charger chaque image de troupe et de sort
       const images = await Promise.all(
@@ -51,38 +53,99 @@ module.exports = {
       // ajouter l'image de fond à la toile
       ctx.drawImage(background, 0, 0);
 
+      // calculer la largeur disponible pour les troupes
+      const troupeWidth = canvas.width - 50 - image2[0].img.width;
+
       // ajouter chaque image de troupe à la toile en fonction de la quantité spécifiée
       let x = 0;
       let y = 50;
+      let maxRowHeight = 0;
       images.forEach(({ count, image }) => {
-        ctx.drawImage(image, x, 0);
+        let width = image.width;
+        let height = image.height;
+
+        // vérifier si l'image dépasse du cadre
+        if (x + width > troupeWidth) {
+          // réduire la largeur de l'image pour qu'elle rentre dans le cadre
+          width = troupeWidth - x;
+
+          // ajuster la hauteur en conséquence
+          height = (width / image.width) * image.height;
+        }
+
+        // vérifier si l'image dépasse du cadre
+        if (x < 0) {
+          // réduire la largeur de l'image pour qu'elle rentre dans le cadre
+          width += x;
+          x = 0;
+        }
+        if (x + width > canvas.width - image2[0].img.width) {
+          width = canvas.width - image2[0].img.width - x;
+        }
+        if (y + height > canvas.height) {
+          height = canvas.height - y;
+        }
+
+        ctx.drawImage(image, x, y, width, height);
         ctx.font = "bold 55px Arial";
         ctx.fillStyle = "#fff"; // blanc
         ctx.textBaseline = "middle";
-        ctx.fillText(count, x + image.width + 5, 0.5 * image.height);
-        x += image.width + 50;
+        ctx.fillText(count, x + width + 5, y + 0.5 * height);
+
+        // vérifier si l'image est la plus haute de la ligne
+        if (height > maxRowHeight) {
+          maxRowHeight = height;
+        }
+
+        // avancer vers la droite pour la prochaine image
+        x += width + 50;
+
+        // si l'image dépasse du cadre, passer à la ligne suivante
+        if (x + width > troupeWidth) {
+          x = 0;
+          y += maxRowHeight + 50; // ajouter un espacement vertical de 50px
+          maxRowHeight = 0; // réinitialiser la hauteur maximale de la ligne
+        }
       });
+      // calculer la hauteur disponible pour chaque image de sort
+      const sortHeight = (canvas.height - 100) / image2.length;
 
       // ajouter chaque image de sort à la toile en fonction de la quantité spécifiée
-      let a = 0;
+      let a = canvas.width - 50;
       let b = 50;
       image2.forEach(({ position, img }) => {
-        ctx.drawImage(img, a, 0);
+        let width = (img.width * sortHeight) / img.height;
+        let height = sortHeight;
+
+        // vérifier si l'image dépasse du cadre
+        if (b < 0) {
+          height += b;
+          b = 0;
+        }
+        if (b + height > canvas.height) {
+          height = canvas.height - b;
+        }
+
+        ctx.drawImage(img, a - width, b, width, height);
         ctx.font = "bold 55px Arial";
         ctx.fillStyle = "#fff"; // blanc
         ctx.textBaseline = "middle";
-        ctx.fillText(position, a + img.width + 5, 0.5 * img.height);
-        a += img.width + 50;
-      });
 
+        // écrire le nombre de sort à côté de l'image
+        ctx.fillText(position, a - width - 50, b + 0.5 * height);
+
+        // avancer vers le bas pour la prochaine image
+        b += height + 1;
+      });
       // obtenir le buffer de l'image finale
       const buffer = canvas.toBuffer();
 
       return buffer;
     }
 
-    // exemple d'utilisation
-    createImage("4x1-2x28-8x83-7x15-s3x5-5x28-1x9-2x10").then((buffer) => {
+    createImage(
+      "4x1-2x28-8x83-7x15-8x0-3x5-2x7-2x11-7x59-8x87-2x2-3x9-s3x5-5x28-1x9-2x10-1x35"
+    ).then((buffer) => {
       const attachment = new MessageAttachment(buffer, "image.png");
       const channel = client.channels.cache.get("1079166164514639882");
 
